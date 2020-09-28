749d8f70c6f7696b22386af940ee0fcb19d08df2
impronik <71666137+impronik@users.noreply.github.com>
2020-09-28 18:31:55 +0200

    lab
    lab_ex1 ,lab_ex2, lab_ex3.

---
+++ lab_ex1.py
@@ -0,0 +1,5 @@
+a = int(input())
+if (a%2==0):
+    print('chetnoe')
+else:
+    print('nechetnoe')
---
+++ lab_ex2.py
@@ -0,0 +1,6 @@
+a1=int(input())
+b= int(input())
+c= int(input())
+for i  in range (1, c + 1, 1):
+    y= a1*b**(i-1)
+    print(y,end = ',')
